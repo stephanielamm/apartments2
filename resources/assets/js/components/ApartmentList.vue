e3954b65c02d52d3c8ebe7dd35e8824f2db759d5
stephanielamm <slamm5@live.unc.edu>
2017-04-28 01:15:15 -0400

    Show Details method working.

--- resources/assets/js/components/ApartmentList.vue
+++ resources/assets/js/components/ApartmentList.vue
@@ -1,15 +1,21 @@
 <template lang="html">
       <div class="ApartmentList">
-
-      <div>
-        <p v-for="apartment in apartments">
-          {{ apartment.name }}
-          {{ apartment.location }}
-          {{ apartment.bedrooms }}
-          {{ apartment.bathrooms }}
-          {{ apartment.price }}
+      <div class="container">
+        <div class="row">
+        <p v-for="apartment in apartments" :class="{ active: isActive(apartment) }">
+          <a href="#" v-on:click="showDetails(apartment.id)">
+            {{ apartment.name }}
+            {{ apartment.location }}
+            {{ apartment.bedrooms }}
+            {{ apartment.bathrooms }}
+            {{ apartment.price }}
+            <p class="conditional">
+              CURRENT APARTMENT
+            </p>
+          </a>
         </p>
       </div>
+    </div>
 
       <!--  v-for, on click it shows a component where you pass the id of the apartment and that component pulls all the info about the apartment or just shows that. -->
       <button>Create Button Goes Here</button>
@@ -39,14 +45,34 @@ export default {
 
   data () {
     return {
+      currentApartment: 0,
       apartments: []
     }
   },
   methods: {
+    showDetails (id) {
+      this.currentApartment = id;
+    },
+    isActive (apartment) {
+      console.log(apartment);
+      if (apartment.id==this.currentApartment) {
+        return true;
+      }
+      else {
+          return false;
+      }
+
+    }
 
   }
 }
 </script>
 
 <style lang="css">
+  .conditional {
+    display: none;
+  }
+  .active .conditional {
+    display: block;
+  }
 </style>
