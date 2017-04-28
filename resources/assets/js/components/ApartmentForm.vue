216fab25b31e79a85fceab5cd56941907d968ed4
stephanielamm <slamm5@live.unc.edu>
2017-04-27 22:38:53 -0400

    Rough input form created.

--- resources/assets/js/components/ApartmentForm.vue
+++ resources/assets/js/components/ApartmentForm.vue
@@ -1,11 +1,46 @@
 <template>
-  <div class="ApartmentForm">
+<!--  <div class="ApartmentForm">
     <input type="text" v-model="name" />
     <input type="text" v-model="location" />
     <input type="text" v-model="bedrooms" />
     <input type="text" v-model="bathrooms" />
     <input type="text" v-model="price" />
-    <button class="btn btn-primary" @click="create">Create Contact</button>
+    <button class="btn btn-primary" @click="create">Create Contact</button> -->
+<!--FORM-->
+<div class="ApartmentForm">
+      <div class="col-md-12">
+        <h1 class="formTitle">Add New Apartment</h1>
+      </div>
+      <div class="col-md-12">
+     <div class="col-md-6">
+       <div class="form-group">
+         <input class="form-control" type="text" placeholder="Community Name" v-model="name" />
+       </div>
+     </div>
+     <div class="col-md-6">
+       <div class="form-group">
+         <input class="form-control" type="text" placeholder="Location" v-model="location" />
+       </div>
+     </div>
+     <div class="col-md-6">
+       <div class="form-group">
+         <input class="form-control" type="text" placeholder="Bedrooms" v-model="bedrooms" />
+       </div>
+     </div>
+     <div class="col-md-6">
+       <div class="form-group">
+         <input class="form-control" type="text" placeholder="Bathrooms" v-model="bathrooms" />
+       </div>
+     </div>
+     <div class="col-md-6">
+       <div class="form-group">
+         <input class="form-control" type="text" placeholder="Price" v-model="price" />
+       </div>
+     </div>
+     <div class="col-md-12">
+          <button class="btn btn-primary pull-right create" @click="create()">create</button>
+    </div>
+   </div>
   </div>
 </template>
 
