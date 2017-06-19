5160b9729492c6e7fcc51c79facb1e492fc91b7c
stephanielamm <slamm5@live.unc.edu>
2017-06-19 15:44:53 -0400

    updated mysql connection

--- config/database.php
+++ config/database.php
@@ -43,22 +43,26 @@ return [
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
         ],
-
-        'mysql' => [
+        
+         'mysql' => [
             'driver' => 'mysql',
-            'host' => $host,
-            'port' => env('DB_PORT', '3306'),
-            'database' => $database,
-            'username' => $username,
-            'password' => $password,
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
+            'host' => env('DB_HOST', '127.0.0.1'),
+            // 'host' => $host,
+            // 'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            // 'database' => $database,
+            // 'username' => $username,
+            // 'password' => $password,
+            // 'unix_socket' => env('DB_SOCKET', ''),
+            // 'charset' => 'utf8mb4',
+            // 'collation' => 'utf8mb4_unicode_ci',
+            // 'prefix' => '',
+            // 'strict' => true,
+            // 'engine' => null,
         ],
-
+        
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
